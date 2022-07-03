8b12999531d3b0eb51bd42c07405cd42d54be0d5
crocyoiin <19chunnw3@gmail.com>
2022-07-03 21:57:23 +1000

    react setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./style.css"
+
+function App() {
+  return (
+    <div>
+    <h1>PAY YOUR DEBTS</h1>
+    <div className="wrap-three">
+      <div className="block block-one">
+        <h2>Title 01</h2>
+        <h3>Subtitle</h3>
+        <p>Something</p>
+      </div>
+      <div className="block block-two">
+        <h2>Title 02</h2>
+        <h3>Subtitle</h3>
+        <p>Something</p>
+      </div>
+      <div className="block block-three">
+        <h2>Title 03</h2>
+        <h3>Subtitle</h3>
+        <p>Something</p>
+      </div>
+    </div>
+    <div className="block block-04">
+      <h2>Title 04</h2>
+      <h3>Subtitle</h3>
+      <p>Something</p>
+    </div>
+    <div className="block block-05">
+      <h2>Title 05</h2>
+      <h3>Subtitle</h3>
+      <p>Something</p>
+    </div>
+  </div>
+  );
+}
+
+export default App;
