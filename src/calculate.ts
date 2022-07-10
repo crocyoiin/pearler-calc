2383df47a6ba812df8bc3bab7c4475a2d9175d78
Vinayak Fialok <z5416144@ad.unsw.edu.au>
2022-07-10 14:31:12 +1000

    Fixed PNI

--- src/calculate.ts
+++ src/calculate.ts
@@ -13,54 +13,59 @@ export interface item {
 }
 
 function monthlyLoanAmount(loanAmount: number, loanTerm: number, rate: number) {
-    return loanAmount * (((rate*(1+rate))**loanTerm) / (((1+rate)**loanTerm)-1));
+    return loanAmount * ((rate*((1+rate)**(loanTerm * 12))) / (((1+rate)**(loanTerm * 12))-1));
 }
 
 export function calcData (userData) { 
     let dataArray: item[] = [];
-    if (userData.repaymentType = 'PNI') {
-        if (userData.investmentType = 'ownerOccupied') {
+    console.log(userData);
+    if (userData.repaymentType == 'PNI') {
+        if (userData.purpose == 'ownerOccupied') {
             for (let i of ownPNI) {
-                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm, i.rate[0]);
+                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm, i.rate[0] / 100 / 12);
+                console.log(`PNI OWNER ${monthly}`);
                 dataArray.push ({
                     name: i.type,
-                    totalInterest: monthly * 12 * userData.loanTerm - userData.loanAmount,
-                    totalPayed: monthly * 12 * userData.loanTerm,
+                    totalInterest: (monthly * 12 * userData.loanTerm) - userData.loanAmount,
+                    totalPayed: (monthly * 12 * userData.loanTerm),
                     monthlyRepayment: monthly
                 });
             }
         } else {
             for (let i of investPNI) {
-                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm, i.rate[0]);
+                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm, i.rate[0] / 100 / 12);
+                console.log(`PNI INVEST ${monthly}`);
                 dataArray.push ({
                     name: i.type,
-                    totalInterest: monthly * 12 * userData.loanTerm - userData.loanAmount,
-                    totalPayed: monthly * 12 * userData.loanTerm,
+                    totalInterest: (monthly * 12 * userData.loanTerm) - userData.loanAmount,
+                    totalPayed: (monthly * 12 * userData.loanTerm),
                     monthlyRepayment: monthly
                 });
             }
         }
     } else {
-        if (userData.investmentType = 'ownerOccupied') {
+        if (userData.purpose == 'ownerOccupied') {
             for (let i of ownInterestOnly) {
-                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm - 5, i.rate[0]);
+                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm - 5, i.rate[0] / 100);
+                console.log(`INTEREST OWNER`);
                 dataArray.push ({
                     name: i.type,
-                    totalInterest: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0] - userData.loanAmount,
-                    totalPayed: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0],
+                    totalInterest: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0] / 100 - userData.loanAmount,
+                    totalPayed: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0] / 100,
                     monthlyRepayment: monthly,
-                    PNIRepayment: userData.loanAmount * i.rate[0] / 12,
+                    PNIRepayment: userData.loanAmount * i.rate[0] / 100 / 12,
                 });
             }
         } else {
             for (let i of investInterestOnly) {
-                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm - 5, i.rate[0]);
+                let monthly = monthlyLoanAmount(userData.loanAmount, userData.loanTerm - 5, i.rate[0] / 100);
+                console.log(`INTEREST INVEST`);
                 dataArray.push ({
                     name: i.type,
-                    totalInterest: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0] - userData.loanAmount,
-                    totalPayed: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0],
+                    totalInterest: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0] / 100 - userData.loanAmount,
+                    totalPayed: monthly * 12 * (userData.loanTerm - 5) + userData.loanAmount * i.rate[0] / 100,
                     monthlyRepayment: monthly,
-                    PNIRepayment: userData.loanAmount * i.rate[0] / 12,
+                    PNIRepayment: userData.loanAmount * i.rate[0] / 100 / 12,
                 });
             }
         }
